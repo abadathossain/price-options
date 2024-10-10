764ff00bfdcef7b68c1f12b77f85c4c488d7c4f3
Abadat Hossain <abadat26@gmail.com>
2024-10-10 12:00:14 +0600

    axios set up completed successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 
+import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [phones, setPhones]=useState([])
+
+ useEffect(()=>{
+  axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+  .then(data=>setPhones(data.data.data))
+ },[])
 
 
   return (
     <>
      
-      <h1 className='text-7xl bg-slate-500'>Vite + React</h1>
-     
+      <h1 className='text-7xl bg-slate-500'>Price Options</h1>
+   <div>
+    {
+      phones.map(phone=><h1>{phone.phone_name}</h1>)
+    }
+   </div>
+         
     </>
   )
 }
